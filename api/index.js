c55ef70024ff463f19a3920cc2832dbf6e7c235f
nipplelion <piolotorrico@yahoo.com>
2024-11-07 20:27:23 -0800

    feat: add initial API structure

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = 3000;
+app.use("/users", routes.user);
+app.use("/comments", routes.comment);
+app.use("/posts", routes.post);
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`TOP-BLOG-API listening on port ${process.env.PORT}!`);
 });
---
+++ api/routes/comment.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/:commentId", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.delete("/:commentId", (req, res) => {});
+
+router.put("/:commentId", (req, res) => {});
+
+module.exports = router;
